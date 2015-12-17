9730d9f0cc8a639aed991ebe33b1a7ab0cfabef6
Christopher Bignamini <bignamic@cresciano.cscs.ch>
2015-12-17 13:46:41 +0100

    Example for div operator added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "DExprIdentity.hpp"
 #include "evaluate.hpp"
 #include "operator_sum.hpp"
+#include "operator_div.hpp"
 #include <iostream>
 
 
@@ -10,11 +11,18 @@ int main()
     DExpr<DExprIdentity> x;
     DExpr<DExprIdentity> y;
 
-    std::cout<<evaluate(1.,3.)<<std::endl;
-    std::cout<<evaluate(x,3.)<<std::endl;
-    std::cout<<evaluate(x+y,3.)<<std::endl;
-    std::cout<<evaluate(x+1.,3.)<<std::endl;
-    std::cout<<evaluate(1.+x,3.)<<std::endl;
-
+    std::cout<<evaluate(1.,3.)<<" "<<1<<std::endl;
+    std::cout<<evaluate(x,3.)<<" "<<3<<std::endl;
+    std::cout<<evaluate(x+y,3.)<<" "<<5<<std::endl;
+    std::cout<<evaluate(x+1.,3.)<<" "<<4<<std::endl;
+    std::cout<<evaluate(1.+x,3.)<<" "<<4<<std::endl;
+    std::cout<<evaluate(1./x,2.)<<" "<<0.5<<std::endl;
+    std::cout<<evaluate(x/2.,3.)<<" "<<1.5<<std::endl;
+    std::cout<<evaluate(x/x,3.)<<" "<<1<<std::endl;
+    std::cout<<evaluate((x+1.)/x,2.)<<" "<<1.5<<std::endl;
+    std::cout<<evaluate(x/(x+1.),3.)<<" "<<0.75<<std::endl;
+    std::cout<<evaluate((1.+x)/x,2.)<<" "<<1.5<<std::endl;
+    std::cout<<evaluate(x/(1.+x),3.)<<" "<<0.75<<std::endl;
+    
   return 0;
 } 
