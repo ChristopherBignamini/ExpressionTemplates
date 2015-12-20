9900e6cd8ffa30c7321b5890b94471d4ca738555
ChristopherBignamini <bignamini@cscs.ch>
2015-12-20 18:56:11 +0100

    Added multiplication case

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "DExprIdentity.hpp"
 #include "evaluate.hpp"
 #include "operator_sum.hpp"
+#include "operator_mult.hpp"
 #include "operator_div.hpp"
 #include "operator_math_fun.hpp"
 #include <iostream>
@@ -9,11 +10,10 @@
 int main()
 {
     ET::DExpr<ET::DExprIdentity> x;
-    ET::DExpr<ET::DExprIdentity> y;
 
     std::cout<<ET::evaluate(1.,3.)<<" "<<1<<std::endl;
     std::cout<<ET::evaluate(x,3.)<<" "<<3<<std::endl;
-    std::cout<<ET::evaluate(x+y,3.)<<" "<<5<<std::endl;
+    std::cout<<ET::evaluate(x+x,3.)<<" "<<5<<std::endl;
     std::cout<<ET::evaluate(x+1.,3.)<<" "<<4<<std::endl;
     std::cout<<ET::evaluate(1.+x,3.)<<" "<<4<<std::endl;
     std::cout<<ET::evaluate(1./x,2.)<<" "<<0.5<<std::endl;
@@ -32,6 +32,8 @@ int main()
     std::cout<<ET::evaluate(ET::exp(ET::log(x)),3.)<<" "<<std::exp(std::log(3.))<<std::endl;
 
     std::cout<<ET::evaluate(ET::exp(x/(x+1.)),3.)<<" "<<std::exp(0.75)<<std::endl;
+
+    std::cout<<ET::evaluate(x+2.*x*ET::exp(x/(x+1.)),3.)<<" "<<3.+2.*3.*std::exp(0.75)<<std::endl;
 
     
   return 0;
